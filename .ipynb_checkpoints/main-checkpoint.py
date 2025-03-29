c88bf911f45deb8f39db14e1f9c687919a870985
yossefsabry <yossefsabry66@gmail.com>
2025-03-29 17:49:59 +0200

    adding the main structure for project and try to update the intents

---
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,12 @@
+
+import os
+import json
+import random
+
+
+def main():
+    print("Hello from chatbot-ai!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ main.py
@@ -0,0 +1,15 @@
+
+import os
+import json
+
+import random
+import nltk
+
+import numpy as np
+
+def main():
+    print("Hello from chatbot-ai!")
+
+
+if __name__ == "__main__":
+    main()
